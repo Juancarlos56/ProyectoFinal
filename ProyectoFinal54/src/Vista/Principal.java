5e9ca5a121a43aebcb7e17b5d3bfcf0e500b2311
Juancarlos56 <51795550+Juancarlos56@users.noreply.github.com>
2019-06-18 00:28:17 -0500

    pull

--- ProyectoFinal54/src/Vista/Principal.java
+++ ProyectoFinal54/src/Vista/Principal.java
@@ -16,7 +16,7 @@ public class Principal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola mundo Prueba Pull      ");
+        System.out.println("Hola mundo Prueba Pullshing      ");
         VentanaLogin v = new VentanaLogin();
         v.setVisible(true);
              
